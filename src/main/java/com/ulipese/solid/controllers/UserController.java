4c471e55949c91106e06d4475f6cf704ebfa3cf9
ulipese <felipe.sousa4030@gmail.com>
2025-02-02 18:53:47 -0300

    refactor(Interfaces): implement interfaces and clean repository unused methods

--- src/main/java/com/ulipese/solid/controllers/UserController.java
+++ src/main/java/com/ulipese/solid/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.ulipese.solid.controllers;
 
 import com.ulipese.solid.entities.User;
+import com.ulipese.solid.interfaces.services.UserServiceInterface;
 import com.ulipese.solid.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,30 +15,30 @@ import java.util.List;
 @Tag(name = "User Controller", description = "This is the user controller, where you can create, search for one or more, and update or delete users")
 public class UserController {
     @Autowired
-    UserService userService;
+    UserServiceInterface userServiceInterface;
 
     @GetMapping
     public List<User> getUsers() {
-        return userService.getAllUsers();
+        return userServiceInterface.getAllUsers();
     }
 
     @GetMapping("/{userId}")
     public User getUser(@PathVariable("userId") String userId) {
-        return userService.getUserById(userId);
+        return userServiceInterface.getUserById(userId);
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
-        return userService.saveUser(user);
+        return userServiceInterface.saveUser(user);
     }
 
     @PutMapping("/{userId}")
     public void updateUser(@PathVariable String userId, @RequestBody User user) {
-        userService.updateUser(userId, user);
+        userServiceInterface.updateUser(userId, user);
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable("userId") String userId) {
-        userService.deleteUser(Integer.parseInt(userId));
+        userServiceInterface.deleteUser(userId);
     }
 }
